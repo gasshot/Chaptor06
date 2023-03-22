c25e1396236dd71fd4c97fc7990973a677f9a4a1
인오 강 <kanginoh@gmail.com>
2023-03-22 15:33:41 +0900

    202303220333

---
+++ RefReturn/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RefReturn
+{
+    class Product // 겟더셋 방식
+    {
+        private int price = 100;
+
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+        public void PrintPrice()
+        {
+            Console.WriteLine($"Price : {price}");
+        }
+    }
+  
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Product carrot = new Product(); // 변수 안에 Product가 객체화
+            ref int ref_local_price = ref carrot.GetPrice();
+            int normal_local_price = carrot.GetPrice();
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal Local Price : {normal_local_price}");
+
+            ref_local_price = 200;
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal Local Price : {normal_local_price}");
+        }
+    }
+}
---
+++ UsingOut/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UsingOut
+{
+    class MainApp
+    {
+        static void Divide(int a, int b, out int quotient, out int remider)
+        {
+            quotient = a / b;
+            remider = a % b;
+        }
+ 
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            //int c;
+            //int d;
+
+            Divide(a, b, out int c, out int d);
+
+            Console.WriteLine($"a:{a}, b:{b}, a/b:{c},a%b:{d}");
+        }
+    }
+}
